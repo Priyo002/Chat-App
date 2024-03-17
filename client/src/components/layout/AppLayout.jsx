1d4bf54ba387fea4373c8f07937a4468f852a664
Priyabrata Das <priyobapi@gmail.com>
2024-03-17 13:09:33 +0530

    Bug Fixed

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -2,41 +2,39 @@ import React from "react";
 import Header from "./Header.jsx";
 import Title from "../shared/Title.jsx";
 import { Grid } from "@mui/material";
-const AppLayout=()=>(WrappedComponent)=>{
+const AppLayout = () => (WrappedComponent)=>{
     return (props)=>{
         return(
             <>
                 <Title/>
                 <Header/>
-                <Grid container height={"calc(100vh-4rem)"}>
+                <Grid container height={"94.074vh"}>
                     <Grid 
                         item 
-                        sm={4} 
-                        md={3} 
+                        sm = {4}
+                        md = {3}
                         sx={{
-                            display:{xs:"none",sm:"block"},
-                        }} 
-                        height={"100%"}
-                    >
+                            display: {xs: "none", sm:"block"},
+                        }}
+                        height={"100%"}>
                         First
                     </Grid>
 
-                    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"} >
-                        <WrappedComponent {...props}/>
+                    <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+                        <WrappedComponent {...props} />
                     </Grid>
 
                     <Grid 
                         item 
                         md={4} 
                         lg={3} 
-                        height={"100%"} 
+                        height={"100%"}
                         sx={{
-                            display:{xs:"none", md:"block"},
-                            padding:"2rem",
-                            bgcolor:"rgba(0,0,0,0.85)",
-                        }}
-                    >
-                        Third
+                            display: {xs: "none", md:"block"},
+                            padding: "2rem",
+                            bgcolor: "rgba(0,0,0,0.85)",
+                        }}>
+                    Third
                     </Grid>
                 </Grid>
             </>
